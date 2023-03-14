2bdf7cc2268783fdeb4b596b299d60311819bc94
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2023-03-15 00:53:33 +0100

    Add upsert method

--- Library.DataAccess/Repository/UnitOfWork.cs
+++ Library.DataAccess/Repository/UnitOfWork.cs
@@ -14,11 +14,15 @@ namespace Library.DataAccess.Repository
         public UnitOfWork(LibraryDBContext db)
         {
             _db = db;
+
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
+            Product = new ProductRepository(_db);          
         }
+
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
+        public IProductRepository Product { get; private set; }
 
         public void Save()
         {
---
+++ Library/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using Library.DataAccess;
+using Library.DataAccess.Repository.IRepository;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace Library.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> CoverTypeList = _unitOfWork.Product.GetAll();
+            return View(CoverTypeList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+
+                TempData["success"] = "A new cover type added succesfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            if (id == null || id == 0)
+            {
+                //create product
+
+                return View(product);
+            }
+            else
+            {
+                //update product
+            }
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+
+                TempData["success"] = "Cover type updated succesfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var product = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+          
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost, ActionName("DeletePost")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Cover type deleted succesfully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
