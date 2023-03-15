cef643bc96278990e70acd04a7aa54e85e09b99a
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2023-03-15 22:57:30 +0100

    Add datatable

---
+++ Library/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "25%" },
+            { "data": "isbn", "width": "25%" },
+            { "data": "price", "width": "25%" },
+            { "data": "author", "width": "25%" }
+        ]
+    });
+}
