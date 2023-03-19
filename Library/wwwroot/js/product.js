5465421d2e7ba8e57373676b88dc543ea72baf3e
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2023-03-19 13:08:46 +0100

    Upgrade sdk6 to sdk 7 and all dependencies in accordance

--- Library/wwwroot/js/product.js
+++ Library/wwwroot/js/product.js
@@ -12,7 +12,7 @@ function loadDataTable() {
         "columns": [
             { "data": "title", "width": "10%" },
             { "data": "isbn", "width": "10%" },
-            { "data": "price", "width": "10%" },
+            { "data": "price", "width": "10%"},
             { "data": "author", "width": "10%" },
             { "data": "category.name", "width": "10%" },
             { "data": "coverType.name", "width": "10%" },
@@ -20,7 +20,7 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `
-                         <div class="w-100 btn-group" role = "group">
+                         <div class="w-100 btn-group" role = "group"> button
                               <a href="/Admin/Product/Upsert?id=${data}"
                                  class="btn btn-secondary mx-2 rounded-2" > <i class="bi bi-pencil-square"></i>&nbsp;&nbsp;Update</a>
                               <a onClick=Delete('/Admin/Product/Delete/${data}')
