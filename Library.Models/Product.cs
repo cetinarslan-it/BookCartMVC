4e387e58b7c4084716d3a5da906d066dac05164e
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2023-03-19 10:03:55 +0100

    Add default lorem ipsum values to product model

--- Library.Models/Product.cs
+++ Library.Models/Product.cs
@@ -13,34 +13,34 @@ namespace Library.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = LoremNET.Lorem.Words(1) + " " +LoremNET.Lorem.Words(1) + " " + LoremNET.Lorem.Words(1);
+        public string Description { get; set; } = LoremNET.Lorem.Paragraph(5, 16, 4, 12);
 
         [Required]
-        public string ISBN { get; set; }
+        public string ISBN { get; set; } = LoremNET.Lorem.Number(1000, 2000).ToString()+" - "+ LoremNET.Lorem.Number(100000, 200000).ToString();
 
         [Required]
-        public string Author { get; set; }
+        public string Author { get; set; } = LoremNET.Lorem.Words(1) + " " + LoremNET.Lorem.Words(1);
 
         [Required]
-        [Range(1,10000)]
+        [Range(1, 10000)]
         [DisplayName("List Price")]
-        public double ListPrice { get; set; }
+        public double ListPrice { get; set; } = LoremNET.Lorem.Number(60, 70);
 
         [Required]
         [Range(1, 10000)]
         [DisplayName("Price of 1-50")]
-        public double Price { get; set; }
+        public double Price { get; set; } = LoremNET.Lorem.Number(50, 60);
 
         [Required]
         [Range(1, 10000)]
         [DisplayName("Price of 51-100")]
-        public double Price50 { get; set; }
+        public double Price50 { get; set; } = LoremNET.Lorem.Number(40, 50);
 
         [Required]
         [Range(1, 10000)]
         [DisplayName("Price of 100+")]
-        public double Price100 { get; set; }
+        public double Price100 { get; set; } = LoremNET.Lorem.Number(30, 40);
 
         [ValidateNever]
         [DisplayName("Image")]
